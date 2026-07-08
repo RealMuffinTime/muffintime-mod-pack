7075ad7df6c9356ccc0a1c6b7b6255963d53cf4e
RealMuffinTime <realmuffintime@gmail.com>
2026-07-08 21:30:39 +0200

    update mods and add basic things - add async, craterlib, tt20, voxy server - add README.md - add github actions - add LICENSE - remove effective and distant horizonts

---
+++ .github/workflows/release.py
@@ -0,0 +1,43 @@
+import os
+import zipfile
+
+version = ""
+modrinthFile = open("modrinth-changelog.md", "w")
+githubFile = open("github-changelog.md", "w")
+envFile = open(os.getenv("GITHUB_ENV"), "a")
+
+
+with open("CHANGELOG.md", "r") as inFile:
+    for line in inFile.readlines():
+        if line.startswith("\n"):
+            break
+        elif line.startswith("## "):
+            version = line.split("[")[1].split("]")[0]
+            envFile.write(f"\nNAME=Version {version} - {line.split(' ')[-1].strip()}")
+            envFile.write(f"\nVERSION={version}")
+        elif line.startswith("### "):
+            modrinthFile.write(line)
+            githubFile.write(line)
+        else:
+            modrinthFile.write(line)
+            githubFile.write(line)
+
+with open("CHANGELOG.md", "r") as inFile:
+    for line in inFile.readlines():
+        if line.startswith("- Support Minecraft version "):
+            githubFile.write(f"\nFor Minecraft version `{line.split('`')[1]}`.")
+            minecraftVersions = line.split('`')[1].split("-")
+            envFile.write(f"\nMINECRAFT_VERSION=>={minecraftVersions[0]} <={minecraftVersions[-1]}")
+            break
+
+# TODO
+zf = zipfile.ZipFile(f"muffintime-mod-pack-{version}.zip", "w")
+for dirname, subdirs, files in os.walk("."):
+    if dirname.startswith(os.path.join(".", ".")):
+        continue
+    if not dirname == ".":
+        zf.write(dirname)
+    for filename in files:
+        if not filename.startswith(".") and not filename.startswith("modrinth") and not filename.startswith("github") and not filename.startswith("muffintime"):
+            zf.write(os.path.join(dirname, filename))
+zf.close()
---
+++ README.md
@@ -0,0 +1,11 @@
+# The MuffinTime Modpack
+
+![The MuffinTime Minecraft](https://cdn.modrinth.com/data/cached_images/1927b556518ad89576aa2ad6c185d649b2683991.png)
+
+This modpack is designed and used for [The MuffinTime Minecraft](https://modrinth.com/server/muffintime-minecraft).
+
+It contains for one the clientside modpack, and for the other all the mods used on the server side.
+
+Included on clientside are some performance mods and QOL-mods, like mini-/worldmap and voicechat.
+
+Included on serverside are also performance mods and also QOL-mods, like voicechat, BlueMap, F-Sit and player statistics.
